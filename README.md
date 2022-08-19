d814d62f5674721e47e7c55cf018daf49770b234
unknown <forrestlanhart@gmail.com>
2022-08-18 23:24:09 -0700

    updated javascript comments and updated but incomplete readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Password-challenge-
+
+# Description
+This application helps the user generate a "secure" randomized password to be used for all of their password needs. Using if statements, for loops, and functions, it generates a unique password that contains lowercase and uppercase letters, numbers, and special characters! 
+
+Screenshot
+![alt text](/assets/images/screenshotupdated.png)
+
+Live Link!
+
+License
+The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use https://choosealicense.com/
+:trophy: The sections listed above are the minimum for a high-quality README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+Badges
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by shields.io. You may not understand what they all represent now, but you will in time.
+Features
+If your project has a lot of features, consider adding a heading called "Features" and list them there.
+Contributing
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// This function is what the user uses to select the length of their password and the password must be between 8 and 128 characters
+// in length
 function getCharacterLength() {
   var length = prompt("How long would you like your password to be? Must be between 8 and 128 characters.");
   if (length < 8) {
@@ -11,10 +12,12 @@ function getCharacterLength() {
   return length;
 }
 
-function useLowerCase() {
-  var lowerCase = window.confirm("Would you like to use lowercase letters?");
-  return lowerCase;
-}
+// function useLowerCase() {
+//   var lowerCase = window.confirm("Would you like to use lowercase letters?");
+//   return lowerCase;
+// }
+
+// The following three functions prompt the user to select these options as a yes or no.
 function useUpperCase() {
   var upperCase = window.confirm("Would you like to use uppercase letters?");
   return upperCase;
@@ -28,7 +31,9 @@ function useSpecialCharacters() {
   return specialCharacters;
 }
 
-function generatePassword(length, lowerCase, upperCase, numeric, specialCharacters) {
+// This function is what randomly generates the password, it will randomly fill in the appropriate characters and any characters
+// that are left to fill in will be filled in with lowercase umbers using the for loop.
+function generatePassword(length, upperCase, numeric, specialCharacters) {
   var password = "";
   var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowers = "abcdefghijklmnopqrstuvwxyz";
@@ -44,10 +49,10 @@ function generatePassword(length, lowerCase, upperCase, numeric, specialCharacte
     password = password + chars[Math.floor(Math.random() * chars.length)];
   }
   var currentLength = password.length;
-   if (lowerCase) { 
+  //  if (lowerCase) { 
     for (var i = 0; i<(length-currentLength); i++) {
       password = password + lowers[Math.floor(Math.random() * lowers.length)];
-    }
+    // }
     
 } 
 return password;
@@ -55,14 +60,14 @@ return password;
 // Write password to the #password input
 function writePassword() {
   var length = getCharacterLength();
-  var lowerCase = useLowerCase();
+  // var lowerCase = useLowerCase();
   var upperCase = useUpperCase();
   var numeric = useNumbers();
   var specialCharacters = useSpecialCharacters();
-  if (!lowerCase && !upperCase && !numeric && !specialCharacters) {
-    lowerCase = window.confirm("You must use at least 1 character type, do you want to use lowercase?");
+  if (!upperCase && !numeric && !specialCharacters) {
+    upperCase = window.confirm("You must use at least 1 character type, do you want to use upperCase?");
   }
-  var password = generatePassword(length, lowerCase, upperCase, numeric, specialCharacters);
+  var password = generatePassword(length, upperCase, numeric, specialCharacters);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
